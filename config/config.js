96bcf214907ecdafaed4ca5b75b756a34025ceb6
panox <panospalos@gmail.com>
2015-11-26 13:54:57 +0000

    mongo production

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'database': 'mongodb://localhost:27017/cyclevision',
+  'database': process.env.MONGOLAB_URI || 'mongodb://localhost/cyclevision',
   'bucket': 'https://s3-eu-west-1.amazonaws.com/cyclevision/'
 };
